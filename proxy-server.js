9f4481f5e22dc506726f47a10d5c06e55a4f104f
Sumit Kumar <iteensumit@gmail.com>
2025-11-07 18:31:39 +0530

    cors

--- proxy-server.js
+++ proxy-server.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import fetch from 'node-fetch';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
+app.options('*', cors()); 
 
 // ✅ Root endpoint (GET)
 app.get('/', async (req, res) => {
