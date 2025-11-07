0c5181a84ef84e268b7222372babed091721ff80
Sumit Kumar <iteensumit@gmail.com>
2025-11-07 17:07:07 +0530

    m

--- proxy-server.js
+++ proxy-server.js
@@ -4,15 +4,38 @@ import fetch from 'node-fetch';
 const app = express();
 app.use(express.json());
 
-app.post('/proxy', async (req, res) => {
-  const url = res.body.url;
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(req.body),
+app.get('/', async (req, res) => {
+  return res.json({
+    status: 'Success',
+    message: 'Proxy server running',
   });
-  const data = await response.text();
-  res.status(response.status).send(data);
+})
+
+app.post('/proxy', async (req, res) => {
+  const url = req.body.url;
+  const method = req.body.method;
+  const body = req.body.body;
+  if (method=='GET') {
+    const response = await fetch(url);
+    const data = await response.text();
+    return res.status(response.status).send(data);
+  } else {
+    const response = await fetch(url, {
+      method: method,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    });
+    const data = await response.text();
+    return res.status(response.status).send(data);
+  }
+  
+});
+
+app.use((req, res) => {
+  return res.status(404).json({
+    status: 'Error',
+    message: 'Route not found',
+  });
 });
 
 app.listen(3000, () => console.log('Proxy running on port 3000'));
